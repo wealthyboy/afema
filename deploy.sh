1ac35632d663375d46ad019ab36ae9407462a5a7
Jacob <jacob.atam@gmail.com>
2025-06-14 03:52:10 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,4 @@
 
 cd /home/forge/afema.com || exit
 
-# Use the full path to git just in case
-/usr/bin/git pull origin master
 
-# Use the full path to composer
-/usr/local/bin/composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-
-# Restart PHP-FPM
-( flock -w 10 9 || exit 1
-    echo 'Restarting FPM...'; sudo -S service php8.2-fpm reload ) 9>/tmp/fpmlock
-
-# Run migrations
-if [ -f artisan ]; then
-    /usr/bin/php artisan migrate --force
-fi
-
-# Build assets
-/usr/bin/npm run prod
