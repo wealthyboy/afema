a7628c3abda0311555c1339d6be0dd441f235828
Jacob <jacob.atam@gmail.com>
2025-06-19 00:16:03 +0100

    fix

--- resources/views/admin/information/edit.blade.php
+++ resources/views/admin/information/edit.blade.php
@@ -14,34 +14,7 @@
             @method('PATCH')
             <div class="card-content">
                <h4 class="card-title">Information</h4>
-               <div class="row">
-                  <div class="col-md-6">
-                     <div class="card">
-                        <div class="card-header text-center">
-                           <h4 class="card-title">Images Only</h4>
-                        </div>
-                        <div class="card-content">
-                           <div id="m_image" class="uploadloaded_image text-center mb-3">
-                              <div class="upload-text {{ $information->image !== null  ?  'hide' : '' }}">
-                                 <a class="activate-file" href="#">
-                                    <img src="{{ asset('backend/img/upload_icon.png') }}">
-                                    <b>Add Image </b>
-                                 </a>
-                              </div>
-                              <div id="remove_image" class="remove_image {{ $information->image !== null  ?  '' : 'hide' }}">
-                                 <a class="delete_image" href="#">Remove</a>
-                              </div>
-                              @if ( $information->image )
-                              <img id="stored_image" class="img-thumnail" src="{{ $information->image }}" alt="">
-                              @endif
-                              <input type="hidden" class="file_upload_input  stored_image" value="{{ $information->image }}" name="image">
-                              <input accept="image/*" class="upload_input" data-msg="Upload  your image" type="file" id="file_upload_input" name="product_image" />
-                           </div>
-                        </div><!-- end content-->
 
-                     </div><!--  end card  -->
-                  </div> <!-- end col-md-4 -->
-               </div>
 
                <div class="row">
 
@@ -63,42 +36,8 @@
                      </div>
                   </div>
                </div>
-               <div class="row">
-                  <div class="col-md-12">
-                     <div class="form-group label-floating is-empty">
-                        <label class="control-label">Custom Link</label>
-                        <input name="custom_link" value="{{ $information->custom_link }}" class="form-control" type="text">
-                        <span class="material-input"></span>
-                     </div>
-                  </div>
-               </div>
-               <div class="row">
-                  <div class="col-md-12">
 
-                     <div class="form-group ">
-                        <label class="control-label"></label>
-                        <select name="parent_id" class="form-control">
-                           <option value="">--Choose One--</option>
 
-                           @foreach($pages as $page)
-
-                           @if($information->parent_id == $page->id)
-                           <option class="" value="{{  $page->id }}" selected="selected">{{ $page->title }} </option>
-                           @continue
-                           @endif
-
-                           @if($page->isParent())
-                           <option class="" value="{{ $page->id }}">{{ $page->title }} </option>
-                           @else
-                           <option class="" value="{{ $page->id }}">&nbsp;&nbsp;&nbsp;{{ $page->title }} </option>
-                           @endif
-
-                           @endforeach
-
-                        </select>
-                     </div>
-                  </div>
-               </div>
 
 
                <div class="row">
